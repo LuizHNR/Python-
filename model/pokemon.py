9f3f8dd1e0547175fcf0c2c4347c48ac74f75a81
LuizHNR <luizhneri12@gmail.com>
2024-09-30 18:44:09 -0300

    model

--- model/pokemon.py
+++ model/pokemon.py
@@ -4,7 +4,17 @@ class Pokemon:
     name = ""
     types = []
     
-    def __init__(self):
-        pass
+    def __init__(self, poke_id = "", name = "", types = []):
+        self.id = poke_id
+        self.name = name
+        self.types = types
+        
+    def __str__(self):
+        return f"""
+    \n-----------Pokemon------------\n
+    Numero da Pokedex: {self.id}\n
+    Nome: {self.name}\n
+    Seu Tipo: {self.types}
+    """
     
     
--- model/types.py
+++ model/types.py
@@ -1,7 +1,12 @@
 from type import Type
 
 class Types:
-    slot = 0
+    slot = ""
     type = Type
     
+    def __init__(self, slot, type):
+        self.slot = slot
+        self.type = type
     
+    def __str__(self):
+        return self.type
